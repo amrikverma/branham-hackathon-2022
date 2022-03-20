e4da6559494cc670fbb0a4bc92ef778035adee09
KaptainKermit <kupresaninmarko2@gmail.com>
2022-03-20 15:06:30 -0700

    more stuff

--- app/src/resources/reviewSourcer.js
+++ app/src/resources/reviewSourcer.js
@@ -6,9 +6,9 @@ const ReviewSourcer = ({ reviews }) => {
       <div className="blog-list">
         {reviews.map(review => (
           <div className="REVIEW_boxContainer" key={review.id} >
-            <h2 style={{textAlign='center'}}>{ review.title }</h2>
-            <p>Written by { review.author } for students at { review.schoolName }</p>
-            <p>{ review.body }</p>
+            <h2 className="REVIEW_titleStyle">{ review.title }</h2>
+            <p className="REVIEW_captionStyle">Written by { review.author } for students at { review.schoolName }</p>
+            <p className="REVIEW_contentStyle">{ review.body }</p>
           </div>
         ))}
       </div>
