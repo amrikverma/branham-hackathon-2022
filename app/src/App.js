62463964742562db526ff1dd64c6224ec46a0b36
amrik <73204325+amrikverma@users.noreply.github.com>
2022-03-20 10:47:40 -0700

    default code

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [counter, SetVariable] = useState(0);
+  useEffect(function test() {
+    console.log("The counter is: " + counter);
+  }, [counter]);
+
+  function doOnClick() {
+    SetVariable(counter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={doOnClick}> Click me </button>
     </div>
-  );
+  )
 }
 
 export default App;
+
