27a644a65ee1214d25938bd3b580052d57c72b6f
KaptainKermit <kupresaninmarko2@gmail.com>
2022-03-20 16:50:57 -0700

    IT FINNALY WORKSgit add .git add .

--- app/src/resources/reviewSourcer.js
+++ app/src/resources/reviewSourcer.js
@@ -6,11 +6,11 @@ const ReviewSourcer = ({ reviews }) => {
     return (
     <div>
     <button><Link to="/makereview">Make a review</Link></button>
-      <div style={{marginLeft:"33%"}}>
+      <div style={{marginLeft:"30%"}}>
         {reviews.map(review => (
             <div className="REVIEW_boxContainer" key={review.id} >
-                <strong><h2 className="REVIEW_titleStyle">{ review.title }</h2></strong>
-                <p className="REVIEW_captionStyle">Written by { review.author } for students at { review.schoolName }</p>
+                <strong><h2 className="REVIEW_titleStyle">{ review.course }</h2></strong>
+                <p className="REVIEW_captionStyle">Written by { review.author } for students at { review.school }</p>
                 <p className="REVIEW_contentStyle">{ review.body }</p>
             </div>
             ))}
