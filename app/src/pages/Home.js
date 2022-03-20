6573825e9859b606d0892db54640fbfdffdb7152
amrik <73204325+amrikverma@users.noreply.github.com>
2022-03-20 13:22:36 -0700

    pic added to homepage + css fix

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,12 +1,13 @@
 
 import '.././App.css';
-
+import homepagepic from './homepagepic.png';
 
 const Home = () => {
     return (
     <div>
-        <header className ="homecolor" style={{color: "padding: 128px 16px"}}>
+        <header className ="homecolor" style={{padding: "128px 16px"}}>
             <h1 style={{color: "white"}}>Home</h1>
+            <img src={homepagepic} alt="homework pic" />
         </header>
     </div>
     )
