730229abe032186b3e6ec968d7217e1849ef1504
KaptainKermit <kupresaninmarko2@gmail.com>
2022-03-20 13:39:28 -0700

    text stuff

--- app/src/pages/schoolreviews.js
+++ app/src/pages/schoolreviews.js
@@ -1,5 +1,14 @@
 const schoolreviews = () => {
-    return (  );
+    return (
+        <div>
+            <h1>Course Reviews!</h1>
+            <p>Read reviews made by other students who are taking or taken the course which you have questions on!</p>
+
+            {/* this part here is gonna be the make a review */}
+
+
+        </div>
+    );
 }
  
 export default schoolreviews;
