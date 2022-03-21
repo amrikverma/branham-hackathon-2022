b73d31799cb65a9e3115972c4037e46f2791a2cb
amrik <73204325+amrikverma@users.noreply.github.com>
2022-03-20 17:14:23 -0700

    quick fix :)

--- app/src/resources/reviewSourcer.js
+++ app/src/resources/reviewSourcer.js
@@ -8,14 +8,15 @@ const ReviewSourcer = ({ reviews }) => {
     <button className="go_to_create_review_button"><Link to="/makereview">Make a review</Link></button>
       <div style={{marginLeft:"30%"}}>
         {reviews.map(review => (
-          <div className="REVIEW_boxContainer" key={review.id} >
-            <h2 style={{textAlign:'center'}}>{ review.course }</h2>
-            <p>Written by { review.author } for students at { review.schoolName }</p>
-            <p>{ review.body }</p>
-          </div>
-        ))}
+            <div className="REVIEW_boxContainer" key={review.id} >
+                <strong><h2 className="REVIEW_titleStyle">{ review.course }</h2></strong>
+                <p className="REVIEW_captionStyle">Written by { review.author } for students at { review.school }</p>
+                <p className="REVIEW_contentStyle">{ review.body }</p>
+            </div>
+            ))}
+        </div>
       </div>
     );
   }
-   
+
   export default ReviewSourcer;
