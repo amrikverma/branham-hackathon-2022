b09b77a152a153c42d7c74846791be92091c2095
amrik <73204325+amrikverma@users.noreply.github.com>
2022-03-20 17:10:48 -0700

    whiteboard done! :)

--- app/src/pages/Whiteboard.js
+++ app/src/pages/Whiteboard.js
@@ -1,14 +1,17 @@
 import '.././App.css';
-
+import Container from './container/Container';
 
 const Whiteboard = () => {
+
     return (
-        <div>
-            <header style={{padding: "5px 16px"}}>
-                <h1 style={{color: "white"}, {fontSize: "30px"}}><center>Whiteboard</center></h1>
-            </header>
-        </div>
-    )
-};
+    <div>
+        <header style={{padding: "5px 16px"}}>
+            <h1 style={{color: "white"}, {fontSize: "30px"}}><center>Whiteboard</center></h1>
+        </header>
+
+        <Container/>
+    </div>
+    );
+  }
   
   export default Whiteboard;
--- app/src/resources/reviewSourcer.js
+++ app/src/resources/reviewSourcer.js
@@ -6,7 +6,7 @@ const ReviewSourcer = ({ reviews }) => {
       <div className="blog-list">
         {reviews.map(review => (
           <div className="REVIEW_boxContainer" key={review.id} >
-            <h2 style={{textAlign='center'}}>{ review.title }</h2>
+            <h2 style={{textAlign:'center'}}>{ review.title }</h2>
             <p>Written by { review.author } for students at { review.schoolName }</p>
             <p>{ review.body }</p>
           </div>
