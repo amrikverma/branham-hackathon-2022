e3c607efd4b75d42b3b8bcb05f13db4b8264147d
amrik <73204325+amrikverma@users.noreply.github.com>
2022-03-20 19:25:12 -0700

    font size

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -9,13 +9,12 @@ const Home = () => {
             <img src={homepagepic} height="500px" alt="homework pic" />
         </header>
 
-        <div className = "BRO_WHAT_IS_THIS" >
+        <div className = "BRO_WHAT_IS_THIS" style={{fontSize: "20px"}}>
             Yamucation is a collaborative effort of 3 high school students (Amrik, Marko, and Yash) to help you excell in school. We have all the resources, which will help you succeed and be on your way to your dream college! <br></br>
             Our website has different tools you can use: 
-            <h4>Whiteboard</h4> Our Whiteboard tab is a place where you can draw or write your different ideas. This can also act as a discrete workspace! 
-            <h4>Calculator</h4> Our Calculator tab is a simple tool you can use to help you with your math homework. We have both numerical and graphing calculators.
-
-            <h4>School Review</h4> Our School Review has information about college courses and AP class review to help students discover what the best course for them are. 
+            <h2><u>Whiteboard</u></h2> Our Whiteboard tab is a place where you can draw or write your different ideas. This can also act as a discrete workspace! 
+            <h2><u>Calculator</u></h2> Our Calculator tab is a simple tool you can use to help you with your math homework. We have both numerical and graphing calculators.
+            <h2><u>School Review</u></h2> Our School Review has information about college courses and schools to help students discover what the best courses and schools for them are. 
         </div>
 
     </div>
